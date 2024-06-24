40ec804cca3822e2a9ee030ed6df6c103fcdb572
Simon Lentz <simonemlentz@gmail.com>
2024-06-24 16:28:33 +0200

    Initial commit

---
+++ lib/tesserax.ex
@@ -0,0 +1,18 @@
+defmodule Tesserax do
+  @moduledoc """
+  Documentation for `Tesserax`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tesserax.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tesserax_test.exs
@@ -0,0 +1,8 @@
+defmodule TesseraxTest do
+  use ExUnit.Case
+  doctest Tesserax
+
+  test "greets the world" do
+    assert Tesserax.hello() == :world
+  end
+end
